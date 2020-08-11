8bc375bfba7f86696e074edae70c83692e06c615
mIvanLvov <69505631+mIvanLvov@users.noreply.github.com>
2020-08-11 11:35:19 +0300

    Update ViewController.swift

--- GitNew/ViewController.swift
+++ GitNew/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     
     var newString = "String"
     
+    var newString2 = "String"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
